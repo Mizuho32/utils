71ecffa387db114c25cc501a620d5f42d37864dc
Mizuho <dummy32@hotmail.co.jp>
2017-08-20 23:04:34 +0900

    zshrc install using installer

--- zsh/install.rb
+++ zsh/install.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-require 'fileutils'
-require 'pathname'
-require 'yaml'
+require_relative '../lib/installer'
 
 SOURCE_NAME = "zshrc"
 TARGET_NAME = ".zshrc"
@@ -12,19 +10,11 @@ BK_DIR 			= "backups"
 BK_LST 			= "backups.yaml"
 CUR  				= Pathname(__FILE__).realpath.dirname
 
-
-if File.exist? DEST then
-
-	FileUtils.mkdir("#{CUR}/#{BK_DIR}/") unless File.exist? "#{CUR}/#{BK_DIR}/"
-	FileUtils.mv(DEST, "#{CUR}/#{BK_DIR}/")
-	File.write("#{CUR}/#{BK_LST}", { TARGET_NAME => DEST }.to_yaml)
-
-end
-
-
-print "\nInstall #{DEST} ? [y/n] >>"
-yn = STDIN.gets.chomp
-
-exit unless yn =~ /^y/
-
-FileUtils.symlink("#{CUR}/#{SOURCE_NAME}", DEST)
+install_sym(
+  source_name:  "zshrc",
+  target_name:  ".zshrc",
+  dest:         "#{ENV['HOME']}/.zshrc",
+  bk_dir:       "backups",
+  bk_lst:       "backups.yaml",
+  cur:          Pathname(__FILE__).realpath.dirname
+) 
