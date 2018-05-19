8bbce403f4785b4de4c4dba67e7e8d8d63f7ad44
Mizuho <dummy32@hotmail.co.jp>
2018-05-19 21:45:22 +0900

    zsh/before_install.rb safe locate vim

--- zsh/before_install.rb
+++ zsh/before_install.rb
@@ -3,6 +3,8 @@
 require 'yaml'
 require 'erb'
 
+require_relative '../lib/util'
+
 unless File.exist?(`which vim`.chomp) then
   $stderr.puts <<-"WARN"
 Vim not installed!!!!
@@ -12,7 +14,12 @@ WARN
 end
 
 vim = "$HOME"
-vimruntime = `locate vim`.split("\n").select{|line| line =~ /vim\d+$/}.first
+#vimruntime = `locate vim`.split("\n").select{|line| line =~ /vim\d+$/}.first
+vimruntime = safe_run_cmd("locate vim") {|ex|
+  puts "#{ex.message}",""
+  print "Where is vim runtime dir? >>"
+  STDIN.gets.chomp
+}.split("\n").select{|line| line =~ /vim\d+$/}.first
 
 if vimruntime.nil? or vimruntime.empty? then
   $stderr.puts "$VIMRUNTIME is empty"
