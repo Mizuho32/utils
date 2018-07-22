a5fc84aa154fdfa004469ab80913272007d5dc4b
Mizuho <dummy32@hotmail.co.jp>
2018-07-22 22:30:46 +0900

    zsh/{before_install, zshrc.tmpl} for fish support and common code separated

--- zsh/before_install.rb
+++ zsh/before_install.rb
@@ -13,9 +13,16 @@ end
 
 vim = "$HOME"
 vimruntime = `locate vim`.split("\n").select{|line| line =~ /vim\d+$/}.first
+rcfile_path = Pathname(__FILE__).expand_path.dirname
 
 if vimruntime.nil? or vimruntime.empty? then
   $stderr.puts "$VIMRUNTIME is empty"
 end
 
-File.write("zshrc", ERB.new(File.read("zshrc.tmpl")).result(binding))
+{
+  zsh:  :zshrc
+  fish: :'config.fish'
+}.each{|sh, bname|
+  common = common(sh, binding)
+  File.write(bname, ERB.new(File.read("#{sh}.tmpl")).result(binding))
+}
