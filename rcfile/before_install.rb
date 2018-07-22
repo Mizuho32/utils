3e1861845bc31e0084d7bda61ed507a39182eea6
Mizuho <dummy32@hotmail.co.jp>
2018-07-22 23:02:42 +0900

    rcfile: support zsh, fish

--- rcfile/before_install.rb
+++ rcfile/before_install.rb
@@ -4,6 +4,7 @@ require 'yaml'
 require 'erb'
 
 require_relative '../lib/util'
+require_relative 'tools'
 
 unless File.exist?(`which vim`.chomp) then
   $stderr.puts <<-"WARN"
@@ -18,17 +19,18 @@ vimruntime = safe_run_cmd("locate vim") {|ex|
   puts "#{ex.message}",""
   print "Where is vim runtime dir? >>"
   STDIN.gets.chomp
-}.split("\n").select{|line| line =~ /vim\d+$/}.first
+}.split("\n").select{|line| line =~ /^(?!.*snap).*vim\d+$/}.first
 rcfile_path = Pathname(__FILE__).expand_path.dirname
+
 
 if vimruntime.nil? or vimruntime.empty? then
   $stderr.puts "$VIMRUNTIME is empty"
 end
 
 {
-  zsh:  :zshrc
-  fish: :'config.fish'
+  zsh:  'zshrc',
+  fish: 'config.fish'
 }.each{|sh, bname|
   common = common(sh, binding)
-  File.write(bname, ERB.new(File.read("#{sh}.tmpl")).result(binding))
+  File.write(bname, ERB.new(File.read("#{bname}.tmpl")).result(binding))
 }
