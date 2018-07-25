2d65c0e6e9f31391fc2c51306af846e70cf605ca
Mizuho <dummy32@hotmail.co.jp>
2018-07-25 21:53:05 +0900

    rcfile: fish_greeting

---
+++ rcfile/fish/fish_greeting.fish
@@ -0,0 +1,23 @@
+function fish_greeting
+set n (set_color -o normal)
+set g (set_color -o green)
+set msg \
+'                                                
+                                                
+'$n'Welcome to fish, the friendly interactive shell 
+'$n'Type '$g'help'$n' for instructions on how to use fish   
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                
+                                                '
+
+fish_logo blue cyan green $msg ロ
+
+end
