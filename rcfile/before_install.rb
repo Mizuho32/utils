04725c80106aed4b4f191ab70ec44a9533605785
Mizuho <mizuho128@gmail.com>
2019-06-03 00:40:31 +0900

    completion download

--- rcfile/before_install.rb
+++ rcfile/before_install.rb
@@ -2,6 +2,7 @@
 
 require 'yaml'
 require 'erb'
+require 'open-uri'
 
 require_relative '../lib/util'
 require_relative 'tools'
@@ -28,10 +29,27 @@ if vimruntime.nil? or vimruntime.empty? then
   $stderr.puts "$VIMRUNTIME is empty"
 end
 
+# Download fish completions
+Dir::mkdir(rcfile_path+"fish/completions/") unless File.exist?(rcfile_path+"fish/completions/")
+
+version = `fish --version`[/(\d\.\d\.\d)/, 1]
+base = "https://raw.githubusercontent.com/fish-shell/fish-shell/Integration_#{version}/share/completions/"
+
+loc = YAML.load_file(rcfile_path + "loc.yaml")
+loc[:type][:sym]
+  .map{|file, sym| file.match /fish\/completions\/(?<filename>[^\.]+\.fish)/}
+  .compact
+  .map{|match| match[:filename] }
+  .each{|filename|  
+    url = "#{base}#{filename}"
+    File.write(rcfile_path+"fish/completions/#{filename}", open(url).read)
+  }
+
 # tmux prefix-key
 key = File.read(rcfile_path + "../tmux/tmux.conf")[/prefix\s+C-(\w)/, 1]
 puts "tmux key if #{key}"
 
+# rcfile generaion
 {
   zsh:  'zshrc',
   fish: 'config.fish'
