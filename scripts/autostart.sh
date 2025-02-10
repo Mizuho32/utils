97d7255ee1e5d3e3a0c3994497c8c4c58d3015ae
Mizuho <mizuho128@gmail.com>
2025-02-10 20:39:03 +0900

    autostart.sh for u728/s

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
+konsole &
+
 sleep 10
 
 audacity &
 sleep 1
 
-pavucontrol &
-sleep 1
+# pavucontrol &
+# sleep 1
 
 thunar &
 sleep 1
 
-konsole &
+discord &
+viber &
 sleep 1
 
 # pactl list sources
-pacmd load-module module-loopback latency_msec=5 source=alsa_input.pci-0000_00_1b.0.analog-stereo
+# pacmd load-module module-loopback latency_msec=5 source=alsa_input.pci-0000_00_1b.0.analog-stereo
 
 # pactl list modules
 # pactl unload-module モジュール番号
