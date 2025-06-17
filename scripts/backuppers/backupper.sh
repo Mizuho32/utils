05c81e229fcd06f1a8e5a1a40941eba8cb0d0e9f
Mizuho <mizuho128@gmail.com>
2025-06-17 19:58:15 +0900

    backupper

--- scripts/backuppers/backupper.sh
+++ scripts/backuppers/backupper.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+#pwd
 source env.sh
+#echo $PATH
+#echo $MISE_DATA_DIR
+#mise --version
+#ruby --version
 RUBYLIB=$RUBYLIB TZ=$TZ HOST=$HOST CID=$CID ruby backupper.rb $PWD/backupper_conf.yaml
