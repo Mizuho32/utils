49b2cd02cbb1d0d023e21a33bb4068d9eb75a651
mizho <mizuho128@gmail.com>
2026-03-28 16:56:38 +0900

    scripts/ollama/start-ollama.sh For RoCm AMD GPU enabled ollama

--- scripts/ollama/start-ollama.sh
+++ scripts/ollama/start-ollama.sh
@@ -1,1 +1,5 @@
-CUDA_VISIBLE_DEVICES=1 OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=$(hostname) $HOME/data/ollama/usr/bin/ollama serve
+#CUDA_VISIBLE_DEVICES=1 OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=$(hostname) $HOME/data/ollama/usr/bin/ollama serve
+
+#ROCR_VISIBLE_DEVICES=0 OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=$(hostname) $HOME/data/ollama/usr/bin/ollama serve
+#HIP_VISIBLE_DEVICES=0 OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=$(hostname) $HOME/data/ollama/usr/bin/ollama serve
+OLLAMA_VULKAN=1 OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=$(hostname) /usr/bin/ollama serve
